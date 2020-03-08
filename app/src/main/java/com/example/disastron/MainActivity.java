b86669ad6b1fa6c65e050208d5014a0acc2dfbc5
Utkichaps <utkarshchhapekar2@gmail.com>
2020-03-08 12:03:10 +0530

    SQLite skeleton added

--- app/src/main/java/com/example/disastron/MainActivity.java
+++ app/src/main/java/com/example/disastron/MainActivity.java
@@ -10,10 +10,12 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     int myItemId;
+    DatabaseHelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
     }
 
     @Override
