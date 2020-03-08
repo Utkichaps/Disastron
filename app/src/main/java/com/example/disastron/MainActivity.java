5d97a60055bb08059b73b434d0641800b8e220a8
Utkichaps <utkarshchhapekar2@gmail.com>
2020-03-08 16:22:03 +0530

    Improved Menu

--- app/src/main/java/com/example/disastron/MainActivity.java
+++ app/src/main/java/com/example/disastron/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.disastron;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -32,22 +33,34 @@ public class MainActivity extends AppCompatActivity {
         switch(myItemId)
         {
             case R.id.home :
-                setContentView(R.layout.activity_main);
-                return true;
+                //setContentView(R.layout.activity_main);
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
             case R.id.facebook:
-                setContentView(R.layout.activity_facebook_share);
-                return true;
+                //setContentView(R.layout.activity_facebook_share);
+                Intent intent1 = new Intent(this,FacebookShare.class);
+                startActivity(intent1);
+                break;
             case R.id.contact:
-                setContentView(R.layout.activity_emergency_contact);
-                return true;
+                //setContentView(R.layout.activity_emergency_contact);
+                Intent intent2 = new Intent(this,EmergencyContact.class);
+                startActivity(intent2);
+                break;
             case R.id.firstaid:
-                setContentView(R.layout.activity_first_aid);
-                return true;
+                //setContentView(R.layout.activity_first_aid);
+                Intent intent3 = new Intent(this,FirstAid.class);
+                startActivity(intent3);
+                break;
             case R.id.settings:
-                setContentView(R.layout.activity_settings);
-                return true;
+                //setContentView(R.layout.activity_settings);
+                Intent intent4 = new Intent(this,Settings.class);
+                startActivity(intent4);
+                break;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
+        return true;
     }
 }
