c7ea0d746df9ed3fc2f8b8c98462efc14ccd558d
Devdatta Joshi <39329663+DraconioSchiffer@users.noreply.github.com>
2020-03-09 15:45:57 +0530

    Update FacebookShare.java

--- app/src/main/java/com/example/disastron/FacebookShare.java
+++ app/src/main/java/com/example/disastron/FacebookShare.java
@@ -1,6 +1,9 @@
 package com.example.disastron;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 
 import android.os.Bundle;
 
