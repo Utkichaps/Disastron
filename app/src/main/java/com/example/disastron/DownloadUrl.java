feed78d09e96bda56906a82c9de42c7ce097eb6b
Kunal-khanwalkar <kunalkhanwalkar@gmail.com>
2020-05-19 22:25:23 +0530

    Added reverse GeoCoding and Alerts scraping

--- app/src/main/java/com/example/disastron/DownloadUrl.java
+++ app/src/main/java/com/example/disastron/DownloadUrl.java
@@ -46,6 +46,7 @@ public class DownloadUrl {
             iStream.close();
             urlConnection.disconnect();
         }
+        Log.d("URLSuccess","URL function complete");
         return data;
     }
 }
